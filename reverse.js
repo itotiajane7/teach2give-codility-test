7455fdfcadfb2b3f20ebb7a7358b3141e09d7680
jane <itotiajane8@gmail.com>
2024-08-13 13:24:15 +0300

    commit

--- reverse.js
+++ reverse.js
@@ -6,9 +6,3 @@ console.log(reversestring("12345"))
 
 
 
-console.log(reversestring("hello"))
-
-console.log(reversestring("javascript"))
-
-
-console.log(reversestring("a man a plan a canal panama"))
