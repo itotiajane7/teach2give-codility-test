8b9720f7f4c81d2c971b3f0ea7fc57eda72022a4
jane <itotiajane8@gmail.com>
2024-08-13 23:04:36 +0300

    second commit

--- filter even numbers.js
+++ filter even numbers.js
@@ -1,4 +1,16 @@
 function filterevennumbers(numbers){
-    let arr = []
-    
+    let arr = [];
+    for(let i= 0; i< numbers.length;i++){
+        if(typeof numbers[i] !== 'number'){
+            console.error("only numbers are allowed");
+            return;
+        }
+        if (numbers[i] % 2 === 0){
+            arr.push(numbers[i]);
+        }
+    }
+    console.log(arr);
 }
+
+filterevennumbers([0,-2]);
+
