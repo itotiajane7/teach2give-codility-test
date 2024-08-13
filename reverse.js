6e266fc04ea62e27a119d03c7b0c0a49b26c12ea
jane <itotiajane8@gmail.com>
2024-08-13 11:56:27 +0300

    teach2give

---
+++ reverse.js
@@ -0,0 +1,14 @@
+function reversestring(string){
+    let reverse=[...string].reverse().join("");
+    return reverse
+}
+console.log(reversestring("12345"))
+
+
+
+console.log(reversestring("hello"))
+
+console.log(reversestring("javascript"))
+
+
+console.log(reversestring("a man a plan a canal panama"))
